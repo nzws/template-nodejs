d4cea5dfb666e5a4e13045f04f3daf1218c84608
nzws <git@nzws.me>
2019-07-13 13:14:59 +0900

    Update deps, fix scripts, etc

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 // This is a dummy JavaScript file for eslint.
 // Please delete this file if you use this template.
 
+const helloES6 = name => {
+  return `Hello, ${name} with ES6!`;
+};
+
 function hello(name) {
-  return `Hello, ${name}!`;
+  return 'hello, ' + name + '!';
 }
 
 hello('@yuzulabo');
+helloES6('@yuzulabo');
